250b83185bb30ca8727b9cdc793834d654ece972
Stack Cache <38466296+stackcache@users.noreply.github.com>
2018-10-05 13:48:29 -0500

    added state (#24)

---
+++ app/containers/State/tests/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { State, mapDispatchToProps } from '../index';
+
+describe('<State />', () => {
+  it('should render', () => {
+    const renderedComponent = shallow(<State />);
+    expect(renderedComponent.contains(<p>Upper: SECOND LINE</p>)).toEqual(true);
+  });
+
+  it('should handle dispatches', () => {
+    expect(mapDispatchToProps('test')).toEqual({ dispatch: 'test' });
+  });
+});
